ec647b71f1e6f3ef1febd0983c68b1ba1ad7741d
AngeloDev-New <angelo.dev.santos@gmail.com>
2024-09-05 00:34:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Prcesso de escuta na porta 68 para capturar OFFER
 # biblioteca "socket" usada para lidar com comunicao em rede 
 import socket
+import struct
+
 # Cria um socket UDP pois e assim o protocolo DHCP, contudo se a ideia for criar um socket TCP ao inves de SOCK_DGRAM usariasse socket.SOCK_STREAM
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Define o endereço e a porta de escuta
@@ -8,18 +10,23 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('0.0.0.0', 68)
 #conecta o socket
 server_socket.bind(server_address)
+# opcao necessari para devolver datagram em <broadcast>
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-#desconecta o soket
-
 print("Aguardado Inicio")
+def OFFER_INJECTOR(offer):
+  magicCookie = b"0x630x820x530x63"
+  offer_injected = b''
+  for byte in offer:
+    offer_injected += byte
+    if magicCookie in offer_injected:
+      
+
 while True:
   try:# Tenta receber dados do cliente
-    data, address = server_socket.recvfrom(4096)  # 4096 é o tamanho do buffer
+    offer, address = server_socket.recvfrom(4096)  # 4096 é o tamanho do buffer
     # Exibe os dados recebidos e o endereço do cliente
-
-            # Opcional: Enviar uma resposta para o cliente
-            response = b"Resposta do servidor TFTP"
-    server_socket.sendto(response, address)
+    server_socket.sendto(OFFER_INJECTOR(offer), address)
+    #desconecta o soket
     server_socket.close()
     break
   except KeyboardInterrupt:
